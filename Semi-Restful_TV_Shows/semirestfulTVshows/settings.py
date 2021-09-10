537d1d2e9a597a5ce2a0c6f029497a8f4d710d52
cpsouvenir22 <74630689+cpsouvenir22@users.noreply.github.com>
2021-09-10 14:15:00 -0400

    Update settings.py

--- Semi-Restful_TV_Shows/semirestfulTVshows/settings.py
+++ Semi-Restful_TV_Shows/semirestfulTVshows/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'szgz$vje7+xg-3$#2i6q0rntzg26_knd752l_0+(k5i4fx$(n)'
+SECRET_KEY = ***************************************************
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
